529f293c38d17f8b6888f2021ee8e69c69ef33fb
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-04-30 21:12:17 +0700

    build: update and clean up dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { ignores: ["node_modules", "lib"], files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { ignores: ["node_modules", "lib"], files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+]);
