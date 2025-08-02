d0ac7411eeb5b15cd585f27440e5ddc9521f229f
Daniel Osvaldo Rahmanto <daniel.rahmanto@gmail.com>
2025-08-02 16:02:59 +0700

    build: dependency update

--- tests/schemas.test.ts
+++ tests/schemas.test.ts
@@ -1,4 +1,4 @@
-import { ZodError } from "zod/v4"
+import { ZodError } from "zod"
 import { ApplicationSettingsZod, ConnectionStringSettingsZod, GeneralSettingZod } from "../src/schemas"
 import { validApplicationSettings, validConnectionStringSettings, validGeneralSetting } from "./fixtures"
 
